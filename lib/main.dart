b1d3527a2ac1746256338dc4672a49ca7edd0846
zhyarJB <zhyar.jabary18@gmail.com>
2024-10-01 00:34:58 +0300

    "Added URL launcher plugin, updated main.dart, and modified various platform-specific files for plugin registration."

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,138 @@
 import 'package:flutter/material.dart';
-import 'package:linktree/utils/linktree.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+void main() {
+  runApp(LinkTreeApp());
+}
+
+class LinkTreeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Link Tree',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LinkTreePage(),
+    );
+  }
+}
+
+class LinkTreePage extends StatefulWidget {
+  @override
+  _LinkTreePageState createState() => _LinkTreePageState();
+}
+
+class _LinkTreePageState extends State<LinkTreePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  final List<Link> links = [
+    Link("GitHub", "https://github.com/zhyarJB"),
+    Link("LinkedIn", "https://www.linkedin.com/in/zhyar-jb-5553b52b3/"),
+    Link("Youtube", "https://www.youtube.com/@zhyarjb8711"),
+    Link("Instagram", "https://www.instagram.com/_zhyar18._/"),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..forward();
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Linktree(),
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title:
+            Text('My LinkTree', style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/personal.jpg'),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Zhyar Samdar",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Expanded(
+              child: ListView.builder(
+                itemCount: links.length,
+                itemBuilder: (context, index) {
+                  return ScaleTransition(
+                    scale: _animation,
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: ElevatedButton(
+                        onPressed: () => _launchURL(links[index].url),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          links[index].title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+class Link {
+  final String title;
+  final String url;
+
+  Link(this.title, this.url);
 }
--- lib/utils/linktree.dart
+++ lib/utils/linktree.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Linktree extends StatelessWidget {
-  const Linktree({super.key});
+// class Linktree extends StatelessWidget {
+//   const Linktree({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: CircleAvatar(
-        radius: 92,
-        backgroundImage: NetworkImage(
-            'https://www.facebook.com/photo?fbid=1664275891062983&set=a.113535412803713'),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: CircleAvatar(
+//         radius: 92,
+//         backgroundImage: NetworkImage(
+//             'https://www.facebook.com/photo?fbid=1664275891062983&set=a.113535412803713'),
+//       ),
+//     );
+//   }
+// }
