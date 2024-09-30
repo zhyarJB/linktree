d9d168394c9fd93ab526ca2f06c148a11cc83990
zhyarJB <zhyar.jabary18@gmail.com>
2024-09-30 21:51:35 +0300

    linktree

---
+++ android/app/src/main/kotlin/com/example/linktree/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.linktree
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/linktree.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Linktree extends StatelessWidget {
+  const Linktree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CircleAvatar(
+        radius: 92,
+        backgroundImage: NetworkImage(
+            'https://www.facebook.com/photo?fbid=1664275891062983&set=a.113535412803713'),
+      ),
+    );
+  }
+}
